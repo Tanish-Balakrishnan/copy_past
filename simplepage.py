16fb31e20d302ac42cd67baf2c81ece55b75c2ab
Tanish-Balakrishnan <88792458+Tanish-Balakrishnan@users.noreply.github.com>
2022-01-11 17:22:14 +0530

    Update simplepage.py

--- simplepage.py
+++ simplepage.py
@@ -17,11 +17,7 @@ lbl.place(x=150, y=50)
 text_area = scrolledtext.ScrolledText(wrap=tk.WORD,width=40, height=8)
   
 text_area.place(x=100, y=190,width=600,height=500)
-
-#txtfld=Entry(window, text="This is Entry Widget", bd=5,bg='pink')
-#txtfld = Text(window, width=20, height=3)
-#txtfld.place(x=90, y=170,width=300,height=500)
-window.title('app')
+window.title('Simplepage
 window.configure(bg='#347DA6')
 window.geometry("800x800+100+100")
 
